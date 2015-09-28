1f41658936e9cb71549dc6233c52c2274720c518
Petra Mille <mille.petra@gmail.com>
2015-09-28 14:40:10 +0200

    Again

---
+++ ATM/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATM
+{
+    public class Admin : User
+    {
+
+        public Admin(string ssn, int pin, string fName, string lName) : base(ssn, pin, fName, lName)
+        {
+           
+        }
+
+        private void InsertMoney()
+        {
+
+        }
+    }
+
+    
+}
---
+++ ATM/BankLogic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATM
+{
+    public class BankLogic
+    {
+        
+        
+
+        public void LogIn(string cardNumber, int pin)
+        {
+            //Checks if valid login. Gets user details and account numbers from database. Creates user and saves in session.
+            //Checks number of login attempt
+            //FindUser()
+            
+           // StoreHistory()
+        }
+
+
+
+        public void GetAccountsById()
+        {
+            //Gets all account names associated with the specifc person id, calls GetAccounts in DbController
+        }
+
+        public void GetAccount()
+        {
+            //Gets details about specific account, calls GetAccount in DbController
+            //switch (accountType)
+            //{
+            //case "saving":
+            //double withDrawmoneyLeftToday = CalculateAmountLeftToday(accountNumber);
+            // SavingAccount account = new SavingAccount();
+           
+            //    default:
+             //Account account = new Account();
+            //        break;
+            //}
+
+
+        }
+        //test 
+
+        public void WithdrawFromAccount()
+        {
+            //string resultMessage = account.WithDrawMoney();
+            //StoreHistory();
+            
+        }
+
+        public void GetAccountInformation()
+        {
+            //GetAccountBalance(accountNumber);
+            //GetAccountHistory(accountNumber);
+        }
+
+
+
+    }
+
+
+}
