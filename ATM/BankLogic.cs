1c8b322ed33bd430d1c2f5b55b9c54f3d1f4025c
niicklas <niicklas@hotmail.com>
2015-09-30 14:30:45 +0200

    subtractfromreceipt

--- ATM/BankLogic.cs
+++ ATM/BankLogic.cs
@@ -414,9 +414,10 @@ namespace ATM
             result[1] = amount;
             return result;
         }
-        public void subtractFromReceipt()
+        public void subtractFromReceipt(int lengthOfReceipt, string atmId)
         {
-
+            string commandLine = $"Update Unit SET Receipt = Receipt - '{lengthOfReceipt}' where ID = '{atmId}'";
+            myController.editSQL(commandLine);
         }
 
     }
